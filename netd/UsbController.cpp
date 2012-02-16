9b19f4117675f3e6a64df92d20eb937f03a109ad
Lalit Maganti <lalitmaganti@gmail.com>
2012-02-16 16:16:36 +0000

    Update netd/UsbController.cpp

--- netd/UsbController.cpp
+++ netd/UsbController.cpp
@@ -12,11 +12,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
+
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -36,35 +38,29 @@ UsbController::~UsbController() {
 }
 
 int UsbController::startRNDIS() {
-	LOGD("Usb RNDIS start");
-	return enableRNDIS(true);
+    LOGD("Usb RNDIS start");
+    return enableRNDIS(true);
 }
 
 int UsbController::stopRNDIS() {
-	LOGD("Usb RNDIS stop");
-	return enableRNDIS(false);
+    LOGD("Usb RNDIS stop");
+    return enableRNDIS(false);
 }
+
 
 int UsbController::enableRNDIS(bool enable) {
-	char ums;
-	int fdums = open("/sys/devices/platform/msm_hsusb/gadget/lun0/file", O_RDWR);
-	read(fdums, &ums, 1);
-	close(fdums);
-	if (ums == '/')
-		return 0;
-
-	char value[20];
-	int fd = open("/sys/module/g_android/parameters/product_id", O_RDWR);
-	int count = snprintf(value, sizeof(value), "%s\n", (enable ? "1364" : "1351"));
-	write(fd, value, count);
-	close(fd);
-	return 0;
-}
+	    char value[20];
+	    int fd = open("/sys/module/g_android/parameters/product_id", O_RDWR);
+	    int count = snprintf(value, sizeof(value), "%s\n", (enable ? "1364" : "1351"));
+	    write(fd, value, count);
+	    close(fd);
+	    return 0;
+	}
 
 bool UsbController::isRNDISStarted() {
     char value[5];
     int fd = open("/sys/module/g_android/parameters/product_id", O_RDONLY);
     read(fd, &value, 5);
     close(fd);
-    return (!strncmp(value,"1364",4) ? true : false);
+    return (!strncmp(value,"6881",4) ? true : false);
 }
