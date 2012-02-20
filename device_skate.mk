ee96e6c8df8e7da1201a5169f845a77ced49cda7
Lalit Maganti <tilal6991@gmail.com>
2012-02-20 20:50:41 +0000

    Fix audio

--- device_skate.mk
+++ device_skate.mk
@@ -102,15 +102,13 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    device/zte/skate/prebuilt/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     device/zte/skate/prebuilt/lib/libcommondefs.so:system/lib/libcommondefs.so \
     device/zte/skate/prebuilt/lib/libloc_api-rpc-qc.so:system/lib/libloc_api-rpc-qc.so
 
 # Audio
 PRODUCT_COPY_FILES += \
     device/zte/skate/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
-    device/zte/skate/prebuilt/lib/hw/audio.primary.skate.so:system/lib/hw/audio.primary.skate.so \
-    device/zte/skate/prebuilt/lib/hw/audio_policy.skate.so:system/lib/hw/audio_policy.skate.so
+    device/zte/skate/prebuilt/lib/hw/audio.primary.skate.so:system/lib/hw/audio.primary.skate.so
 
 # Kernel Modules
 PRODUCT_COPY_FILES += \
